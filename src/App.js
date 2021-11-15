fa5c4e2738d2cf63c67f8438dd203c660044d95c
Hunglikeabee <felix_256@hotmail.com>
2021-11-15 12:25:13 +0100

    justtesting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return (
+    <Navigation />
+  );
+}
+
+export default App;
